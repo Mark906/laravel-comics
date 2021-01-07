72b19ce0799cd9842d3aa091902051ee4f815750
Marco <markoka1@outlook.it>
2021-01-07 17:37:02 +0100

    jumbotron + recupero dati da comics.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    @include('partials.jumbotron')
+    <section id="series">
+        <div class="container">
+            @foreach ($series as $element)
+                <div class="comics">
+                    <div class="img">
+                        <img src="{{ $element['thumb'] }}" alt="{{ $element['series'] }}">
+                    </div>
+                    <div class="title">
+                        <h2>{{ $element['series'] }}</h2>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </section>
 @endsection
