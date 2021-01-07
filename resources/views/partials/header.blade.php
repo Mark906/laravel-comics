dd6084ba5765d061f0e8eb00f5b7a1250e687c3d
Marco <markoka1@outlook.it>
2021-01-07 16:17:47 +0100

    Inizializzazione header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,62 @@
+<header>
+    <div class="header-top">
+        <div class="container">
+            <nav>
+                <ul>
+                    <li>
+                        <a class="uppercase" href="#">DC Power Visa</a>
+                    </li>
+                    <li>
+                        <a class="uppercase" href="#">Additional DC Sites</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <nav>
+                <div class="logo">
+                    <img src="{{ asset('images/dc-logo.png') }}" alt="">
+                </div>
+                <div class="main-menu">
+                    <ul>
+                        <li>
+                            <a class="uppercase" href="#">Characters</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Comics</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Movies</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Tv</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Games</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Collectibles</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Video</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Fans</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">New</a>
+                        </li>
+                        <li>
+                            <a class="uppercase" href="#">Shop</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="search">
+
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
