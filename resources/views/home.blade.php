4447b8f5c85d98ad2bfe8fd12be48a87be389b7a
Marco <markoka1@outlook.it>
2021-01-10 21:39:27 +0100

    Aggiunta rotta pagina dettagli con commenti. Impostato il content della pagina dettaglio (quindi il layout). Collegamento link ai relativi id per la pagina dettaglio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,25 @@
     @include('partials.jumbotron')
     <section id="series">
         <div class="container">
+            <div class="section-title">
+                <h2 class="uppercase">Current Series</h2>
+            </div>
             <div class="card-container">
-                @foreach ($series as $element)
+                @foreach ($series as $index => $item)
                     <div class="comics">
-                        <div class="img">
-                            <img src="{{ $element['thumb'] }}" alt="{{ $element['series'] }}">
-                        </div>
-                        <div class="title">
-                            <h2>{{ $element['series'] }}</h2>
-                        </div>
+                        <a href="{{ route('comic-details', ['id' => $index]) }}">
+                            <div class="img">
+                                <img src="{{ $item['thumb'] }}" alt="{{ $item['series'] }}">
+                            </div>
+                            <div class="comic-name">
+                                <span class="uppercase">
+                                    {{ $item['series'] }}
+                                </span>
+                            </div>
+                        </a>
                     </div>
                 @endforeach
             </div>
-
         </div>
     </section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,20 @@ Route::get('/', function () {
     ];
     return view('home', $data);
 })->name('homepage');
+
+Route::get('/comic/{id}', function($id) { //id sarà un parametro dinamico.
+
+    // recupero tutti i fumetti e li salvo in $comics.
+    $comics = config('comics');
+    // verifico che l'id sia una chiave valida
+    if(array_key_exists($id, $comics)) { // cerco nell'array comics l'id.
+
+        // recupero il fumetto corrispondente all'id presente nell'url.
+        $fumetto = $comics[$id];
+        $data = [ // passo 'comic' alla view attraverso $data, contenente l'array $fumetto.
+            'comic' => $fumetto
+        ];
+        return view('single', $data);
+    }
+    abort(404); // se non entra nell'if allora via 'abort' mostro la pagina errore 404.
+})->name('comic-details');
